b07778ab908cac1aaed3a6911b6946235864e09c
Daniel Hentzschel <5008270+DHentzschel@users.noreply.github.com>
2019-05-18 23:50:08 +0200

    Added documentation for SimpleCore and SimpleNetwork

--- doc/SimpleCore/html/search/all_b.js
+++ doc/SimpleCore/html/search/all_b.js
@@ -1,8 +1,8 @@
 var searchData=
 [
   ['mathutils',['MathUtils',['../class_math_utils.html',1,'']]],
-  ['mid',['mid',['../class_a_list.html#a9ccaadf22f9f08197fee08bf6befcfdd',1,'AList::mid()'],['../class_a_vector.html#ac0fd7bd21468b282eb563badd30f73f6',1,'AVector::mid()']]],
+  ['mid',['mid',['../class_a_list.html#a7b9c07f197f9017a7d231a8cf2f19f7b',1,'AList::mid()'],['../class_a_vector.html#a6edc7065ff507e44537fd6e9053f98bf',1,'AVector::mid()']]],
   ['minute',['minute',['../struct_date_time_info.html#a9cf861937714a5b34d7bf574e0c33636',1,'DateTimeInfo']]],
   ['month',['month',['../struct_date_time_info.html#a3109dfcfba10cf250aef5c9cee9a2567',1,'DateTimeInfo']]],
-  ['move',['move',['../class_a_list.html#af091a1d7ddb9a3b9becc827da8eded09',1,'AList::move()'],['../class_a_vector.html#af045120312bc7a66ab31684bdbb039bd',1,'AVector::move()']]]
+  ['move',['move',['../class_a_list.html#ac9979a4b71ae418f1ba7a36b2d9c1a77',1,'AList::move()'],['../class_a_vector.html#a4a3c6ccac7533cee9e2e2a0fddde0d77',1,'AVector::move()']]]
 ];
