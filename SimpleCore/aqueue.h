d4458a4275f29c9048c0375d4b84eaea502a664b
Daniel Hentzschel <5008270+DHentzschel@users.noreply.github.com>
2019-08-07 18:41:38 +0200

    Added documentation to the AQueue class

--- SimpleCore/aqueue.h
+++ SimpleCore/aqueue.h
@@ -4,29 +4,62 @@
 #include "alist.h"
 #include "iqueue.h"
 
+/**
+ * This class implements the IQueue interface and an advanced queue data structure.
+ *
+ * @author Daniel Hentzschel on 07.08.2019.
+ */
 template<class T>
-class Queue : protected AList<T>, public IQueue<T> {
+class AQueue : protected AList<T>, public IQueue<T> {
+public:
+	/**
+	  * Initializes the class with an empty queue.
+	  */
+	AQueue();
+
+	/**
+	  * Enqueues the specified element to the queue.
+	  *
+	  * @param right the enqueuable template class
+	  */
 	void enqueue(const T& value) override;
 
+	/**
+	  * Dequeues the first element from the queue and returns it.
+	  *
+	  * @param right the enqueuable template class
+	  * @return the first element
+	  */
 	T dequeue() override;
 
+	/**
+	  * Returns whether the queue is empty.
+	  *
+	  * @return whether the size of the list behind is empty
+	  */
 	bool isEmpty() override;
 };
 
 template<class T>
-inline void Queue<T>::enqueue(const T& value)
+inline AQueue<T>::AQueue() :
+	AList<T>()
+{
+}
+
+template<class T>
+void AQueue<T>::enqueue(const T& value)
 {
 	AList<T>::append(value);
 }
 
 template<class T>
-inline T Queue<T>::dequeue()
+T AQueue<T>::dequeue()
 {
 	return AList<T>::takeFirst();
 }
 
 template<class T>
-inline bool Queue<T>::isEmpty()
+bool AQueue<T>::isEmpty()
 {
 	return AList<T>::size() == 0;
 }
