d98158bfff05826a273dd98451270fdc161afd71
Daniel Hentzschel <5008270+DHentzschel@users.noreply.github.com>
2019-08-07 18:41:26 +0200

    Added documentation to the IQueue class.

--- SimpleCore/iqueue.h
+++ SimpleCore/iqueue.h
@@ -1,13 +1,37 @@
 #ifndef IQUEUE_H
 #define IQUEUE_H
 
+/**
+ * This class is an interface for queue data structures.
+ *
+ * @author Daniel Hentzschel on 07.08.2019.
+ */
 template <class T>
 class IQueue {
 public:
-	virtual void enqueue(T right) = 0;
+	/**
+	  * Enqueues the specified element to the queue.
+	  * Must be overridden.
+	  *
+	  * @param right the enqueuable template class
+	  */
+	virtual void enqueue(const T& right) = 0;
 
+	/**
+	  * Dequeues the first element from the queue and returns it.
+	  * Must be overridden.
+	  *
+	  * @param right the enqueuable template class
+	  * @return the first element
+	  */
 	virtual T dequeue() = 0;
 
+	/**
+	  * Returns whether the queue is empty.
+	  * Must be overridden.
+	  *
+	  * @return whether the size of the list behind is empty
+	  */
 	virtual bool isEmpty() = 0;
 };
 
