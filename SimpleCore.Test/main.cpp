62f04fe89800ef16138fd220a273e26463311b64
Daniel Hentzschel <5008270+DHentzschel@users.noreply.github.com>
2019-08-21 00:37:28 +0200

    Added test calls in main of project SimpleCore.Test.

--- SimpleCore.Test/main.cpp
+++ SimpleCore.Test/main.cpp
@@ -1,9 +1,18 @@
 #include <cstdlib>
+#include "aqueuetest.cpp"
 #include "vectortest.cpp"
+#include "stopwatchtest.cpp"
 
 int main(int argc, char** argv)
 {
 	VectorTest vectorTest;
 	vectorTest.testAll();
+
+	StopwatchTest stopwatchTest;
+	stopwatchTest.testAll();
+
+	QueueTest queueTest;
+	queueTest.testAll();
+
 	return EXIT_SUCCESS;
 }
